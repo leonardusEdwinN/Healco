fc01492c5385a40fd27739d61561606cbc411a2c
erikson45 <43662385+erikson45@users.noreply.github.com>
2021-06-10 17:52:19 +0700

    create FoodRecog VC

---
+++ Healco/FoodRecogVC.swift
@@ -0,0 +1,22 @@
+//
+//  FoodRecogVC.swift
+//  Healco
+//
+//  Created by Ericson Hermanto on 10/06/21.
+//
+
+import Foundation
+
+
+import UIKit
+
+class FoodRecogVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
