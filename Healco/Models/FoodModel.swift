2c678fafebb36612ef987f3c9a9054db43a06e35
Kelny Tan <bluecosmos94@gmail.com>
2021-06-24 17:10:15 +0700

    Update FoodModel

--- Healco/Models/FoodModel.swift
+++ Healco/Models/FoodModel.swift
@@ -9,7 +9,7 @@
  import UIKit
 
  struct FoodModel{
-    var foodID: Int!
+    var foodID: String!
     var foodName: String!
     var foodDescription: String!
     var foodPhoto: String!
@@ -21,6 +21,9 @@
     var foodSaturatedFat: Double!
     var foodStatus: String!
     var dateTaken: Date!
+    var eatCause: String!
+    var eatFeeling: String!
+    var timeTaken: String!
  }
 
 struct FoodModel2{
@@ -124,6 +127,7 @@ func calculateFood(foodModel : FoodModel2) -> HealthyStatus{
 
  func addDataToModel()->[FoodModel]{
      var model: [FoodModel] = []
+    /* Nanti data2 dari API dimasukkik ke array model terlebih dahulu dan sesuaikan dengan struktur dari struct FoodModel*/
      /*let item1: FoodModel = FoodModel(foodName: "aslnbvsdk", foodDescription: "adjkbfd", foodCalories: 100, foodFat: 67.8, foodCarbohydrate: 87, foodProtein: 75, foodStatus: HealthyStatus.healthy.rawValue)
      let item2: FoodModel = FoodModel(foodName: "aEdbcned", foodDescription: "Qslkdvbnds", foodCalories: 150, foodFat: 92, foodCarbohydrate: 120, foodProtein: 150, foodStatus: HealthyStatus.unhealthy.rawValue)
      let item3: FoodModel = FoodModel(foodName: "Aaklsdvbned", foodDescription: "ewfQdkvn", foodCalories: 200, foodFat: 50.8, foodCarbohydrate: 100, foodProtein: 55, foodStatus: HealthyStatus.common.rawValue)
@@ -144,14 +148,19 @@ func calculateFood(foodModel : FoodModel2) -> HealthyStatus{
      let managedContext = appDelegate.persistentContainer.viewContext
      let entity = NSEntityDescription.entity(forEntityName: "Foods", in: managedContext)!
      for(i) in models.indices{
-         let food = NSManagedObject(entity: entity, insertInto: managedContext)
-         food.setValue(models[i].foodName, forKeyPath: "foodName")
-         food.setValue(models[i].foodDescription, forKeyPath: "foodDescription")
-         food.setValue(models[i].foodCalories, forKeyPath: "foodCalories")
-         food.setValue(models[i].foodFat, forKeyPath: "foodFat")
-         food.setValue(models[i].foodCarbohydrate, forKeyPath: "foodCarbohydrate")
-         food.setValue(models[i].foodProtein, forKeyPath: "foodProtein")
-         food.setValue(models[i].foodStatus, forKeyPath: "foodStatus")
+        let food = NSManagedObject(entity: entity, insertInto: managedContext)
+        food.setValue(models[i].foodName, forKeyPath: "foodName")
+        food.setValue(models[i].foodDescription, forKeyPath: "foodDescription")
+        food.setValue(models[i].foodCalories, forKeyPath: "foodCalories")
+        food.setValue(models[i].foodFat, forKeyPath: "foodFat")
+        food.setValue(models[i].foodCarbohydrate, forKeyPath: "foodCarbohydrate")
+        food.setValue(models[i].foodProtein, forKeyPath: "foodProtein")
+        food.setValue(models[i].foodStatus, forKeyPath: "foodStatus")
+        food.setValue(models[i].foodID, forKeyPath: "foodID")
+        food.setValue(models[i].dateTaken, forKeyPath: "dateTaken")
+        food.setValue(models[i].eatCause, forKeyPath: "eatCause")
+        food.setValue(models[i].eatFeeling, forKeyPath: "eatFeeling")
+        food.setValue(models[i].timeTaken, forKeyPath: "timeTaken")
      }
      do{
          try managedContext.save()
@@ -175,39 +184,51 @@ func calculateFood(foodModel : FoodModel2) -> HealthyStatus{
      return data
  }
 
- func putDataIntoArray()->[FoodModel]{
-     let foodData: [NSManagedObject] = fetchDataFromFoodCoreData()
-     var array: [FoodModel] = []
-     for(i) in foodData.indices{
+func putDataIntoArray()->[FoodModel]{
+    let foodData: [NSManagedObject] = fetchDataFromFoodCoreData()
+    var array: [FoodModel] = []
+    for(i) in foodData.indices{
          /*array.append(FoodModel(foodName: foodData[i].value(forKeyPath: "foodName") as? String, foodDescription: foodData[i].value(forKeyPath: "foodDescription" ) as? String, foodCalories: foodData[i].value(forKeyPath: "foodCalories") as? Double, foodFat: foodData[i].value(forKeyPath: "foodFat") as? Double, foodCarbohydrate: foodData[i].value(forKeyPath: "foodCarbohydrate") as? Double, foodProtein: foodData[i].value(forKeyPath: "foodProtein") as? Double, foodStatus: foodData[i].value(forKeyPath: "foodStatus") as? String))*/
-     }
-     return array
+    }
+    return array
+}
+
+func getFoodFromID(ID: String) -> FoodModel{
+    var food: FoodModel?
+    let foodData = putDataIntoArray()
+    for(i) in foodData.indices{
+        if(foodData[i].foodID == ID){
+            food = foodData[i]
+            return food!
+        }
+    }
+    return food!
+}
+
+func getFoodFromName(name: String)->FoodModel
+{
+    var food: FoodModel?
+    let foodData = putDataIntoArray()
+    for(i) in foodData.indices{
+        if(foodData[i].foodName == name){
+            food = foodData[i]
+            return food!
+        }
+    }
+    return food!
  }
 
- func getFoodFromName(name: String)->FoodModel
- {
-     var food: FoodModel?
-     let foodData = putDataIntoArray()
-     for(i) in foodData.indices{
-         if(foodData[i].foodName == name){
-             food = foodData[i]
-             return food!
-         }
-     }
-     return food!
- }
+public func deleteRequest(){
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
- public func deleteRequest(){
-     let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    let managedContext = appDelegate!.persistentContainer.viewContext
+    let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Foods")
+    let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
 
-     let managedContext = appDelegate!.persistentContainer.viewContext
-     let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Foods")
-     let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-
-     do {
-         try managedContext.execute(deleteRequest)
-     } catch let error as NSError {
-         // TODO: handle the error
+    do {
+        try managedContext.execute(deleteRequest)
+    } catch let error as NSError {
+        // TODO: handle the error
         print(error)
-     }
+    }
  }
