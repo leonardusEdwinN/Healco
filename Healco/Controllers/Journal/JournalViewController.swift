3044b2874c0dcd4b55066bf9bb3a326aeca61225
Edwin <osproject78@gmail.com>
2021-07-30 19:01:55 +0700

    Penyesuaian UI

--- Healco/Controllers/Journal/JournalViewController.swift
+++ Healco/Controllers/Journal/JournalViewController.swift
@@ -11,28 +11,50 @@ import CoreData
 
 class JournalViewController : UIViewController{
     
-    @IBOutlet weak var imageAddJournal: UIImageView!
-    @IBOutlet weak var viewEmptyJournal: UIView!
-    @IBOutlet weak var labelPieChartDetail: UILabel!
-    @IBOutlet weak var labelPieChartPercentage: UILabel!
-    @IBOutlet weak var stackPieChart: UIStackView!
-    @IBOutlet weak var stackNoChartData: UIStackView!
-    @IBOutlet weak var buttonChangeDate: UIButton!
-    @IBOutlet weak var collectionViewPhotoGallery: UICollectionView!
-    @IBOutlet weak var viewSlideShowGallery: UIView!
-    @IBOutlet weak var viewSummary: UIView!
-    @IBOutlet weak var viewPieChart: UIView!
-    @IBOutlet weak var collectionViewWeekly: UICollectionView!
-    @IBOutlet weak var labelCommon: UILabel!
-    @IBOutlet weak var labelUnhealthy: UILabel!
-    @IBOutlet weak var labelHealthy: UILabel!
-    @IBOutlet weak var labelDate: UILabel!
-    var pieChartView = PieChartView()
     
-    //Photo Gallery
-    var carouselData = [Photo]()
-    let cellScale : CGFloat = 0.6
-    var picTakenDetail: UIImage!
+    //Header
+    @IBOutlet weak var viewJournalHeader: UIView!
+    @IBOutlet weak var labelJournalTitle: UILabel!
+    @IBOutlet weak var labelBulan: UILabel!
+    @IBOutlet weak var buttonChangeDate: UIButton!
+    
+    //Weekly CollectionView
+    @IBOutlet weak var collectionViewWeekly: UICollectionView!
+    
+    //Nutrisi hari ini
+    @IBOutlet weak var labelNutrisiHariIni: UILabel!
+    @IBOutlet weak var viewKalori: UIView!
+    @IBOutlet weak var labelKaloriText: UILabel!
+    @IBOutlet weak var labelKalori: UILabel!
+    @IBOutlet weak var labelSatuanKalori: UILabel!
+    
+    //Karbohidrat
+    @IBOutlet weak var labelKarbohidratValue: UILabel!
+    @IBOutlet weak var labelKarbohidratText: UILabel!
+    @IBOutlet weak var viewKarbohidrat: UIView!
+    //Protein
+    @IBOutlet weak var labelProteinValue: UILabel!
+    @IBOutlet weak var labelProteinText: UILabel!
+    @IBOutlet weak var viewProtein: UIView!
+    //Lemak
+    @IBOutlet weak var labelLemakValue: UILabel!
+    @IBOutlet weak var labelLemakText: UILabel!
+    @IBOutlet weak var viewLemak: UIView!
+    
+    //Konsumsimu Hari Ini
+    @IBOutlet weak var labelKonsumsimuHariIni: UILabel!
+    //CollectionView Sarapan
+    @IBOutlet weak var labelSarapan: UILabel!
+    @IBOutlet weak var collectionViewSarapan: UICollectionView!
+    //CollectionView Makan Siang
+    @IBOutlet weak var labelMakanSiang: UILabel!
+    @IBOutlet weak var collectionViewMakanSiang: UICollectionView!
+    //CollectionView Makan Malem
+    @IBOutlet weak var labelMakanMalam: UILabel!
+    @IBOutlet weak var collectionViewMakanMalam: UICollectionView!
+    //CollectionView Snack
+    @IBOutlet weak var labelSnack: UILabel!
+    @IBOutlet weak var collectionViewSnack: UICollectionView!
     
     // Weekly CollectionCell
     var date : [String] = []
@@ -51,46 +73,29 @@ class JournalViewController : UIViewController{
     
     var fetchData: [NSManagedObject] = []
     
+    //progressBar
+    private let progressView : UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .bar)
+        progressView.trackTintColor = .gray
+        progressView.progressTintColor = .green
+        return progressView
+    }()
     
-    var count_healthy: Int = 0
-    var count_common: Int = 0
-    var count_unhealthy: Int = 0
-    var dataEntries : [PieChartDataEntry] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getDate()
         
-        // add tap gesture to image
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.goToFoodRecog))
-        
-        imageAddJournal.isUserInteractionEnabled = true
-        imageAddJournal.addGestureRecognizer(tapGestureRecognizer)
-        buttonChangeDate.layer.cornerRadius = 13.5
-        buttonChangeDate.layer.masksToBounds = true
-        
-        //pieChart
-        pieChartView.delegate = self
-        
-        // UIcollectionview
-        collectionViewPhotoGallery.register(UINib.init(nibName: "GalleryPhotoCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "galleryPhotoCell")
-        collectionViewPhotoGallery.delegate = self
-        collectionViewPhotoGallery.dataSource = self
-        //        let screenSize = UIScreen.main.bounds.size
-        //        let cellWidth = floor(screenSize.width * cellScale)
-        //        let cellHeight = floor(screenSize.height * cellScale)
-        //        let insetX = (collectionViewPhotoGallery.frame.size.width - cellWidth) / 2.0
-        //        let insetY = (collectionViewPhotoGallery.frame.size.height - cellHeight) / 2.0
-        //
-        //        let layout = collectionViewPhotoGallery.collectionViewLayout as! UICollectionViewFlowLayout
-        //        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
-        //        collectionViewPhotoGallery.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
-        
+        viewJournalHeader.layer.cornerRadius = 30
+        viewKarbohidrat.layer.cornerRadius = 15
+        viewProtein.layer.cornerRadius = 15
+        viewLemak.layer.cornerRadius = 15
+        viewKalori.layer.cornerRadius = 15
         
         //weekly
         collectionViewWeekly.register(UINib.init(nibName: "WeeklyCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "weeklyCollectionViewCell")
         collectionViewWeekly.delegate = self
         collectionViewWeekly.dataSource = self
+        
         collectionViewWeekly.allowsMultipleSelection = false
         
         let weekday = Calendar.current.component(.weekday, from: Date())
@@ -114,62 +119,9 @@ class JournalViewController : UIViewController{
         let formattedDate = format.string(from: date)
         fetchData = myFetchRequestByDate(date: formattedDate)
         
-        for(i) in fetchData.indices{
-            let status = fetchData[i].value(forKeyPath: "foodStatus") as? String
-            
-            carouselData.append(Photo(image: UIImage(data: fetchData[i].value(forKeyPath: "foodPhoto") as! Data)!, title: "\(fetchData[i].value(forKeyPath: "foodName") as! String)", description: "\(fetchData[i].value(forKeyPath: "foodDescription") as! String)", status: status!))
-            
-            if(status == "Healthy"){
-                count_healthy += 1
-            } else if status == "Common"{
-                count_common += 1
-            } else {
-                count_unhealthy += 1
-            }
-        }
-        
-        labelHealthy.text = "\(count_healthy) Healthy"
-        labelCommon.text = "\(count_common) Common"
-        labelUnhealthy.text = "\(count_unhealthy) Unhealthy"
-        
-        let totalData = count_healthy + count_common + count_unhealthy
-        if(count_healthy > count_common && count_healthy > count_unhealthy){
-            //makan sehat
-            labelPieChartPercentage.text = "\( round((Double(count_healthy) / Double(totalData)) * 100))%"
-            labelPieChartDetail.text = "Healthy"
-        }else if(count_common > count_healthy && count_common > count_unhealthy){
-            //common
-            
-            labelPieChartPercentage.text = "\( round((Double(count_common) / Double(totalData)) * 100))%"
-            labelPieChartDetail.text = "Common"
-        } else {
-            labelPieChartPercentage.text = "\( round((Double(count_unhealthy) / Double(totalData)) * 100))%"
-            labelPieChartDetail.text = "Unhealthy"
-        }
-        
-        if (carouselData.isEmpty){
-            stackNoChartData.isHidden = false
-            stackPieChart.isHidden = true
-            pieChartView.isHidden = true
-            viewEmptyJournal.isHidden = false
-        } else {
-            stackNoChartData.isHidden = true
-            stackPieChart.isHidden = false
-            pieChartView.isHidden = false
-            viewEmptyJournal.isHidden = true
-        }
     }
     
     
-    func getDate(){
-        let date = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .full
-        dateFormatter.timeStyle = .none
-        
-        let result = dateFormatter.string(from: date)
-        labelDate.text = result
-    }
     
     @IBAction func buttonChangeDateClicked(_ sender: UIButton) {
         
@@ -213,7 +165,7 @@ class JournalViewController : UIViewController{
         dateFormatter.timeStyle = .none
         
         if let date = sender?.date {
-            labelDate.text = dateFormatter.string(from: date)
+//            labelDate.text = dateFormatter.string(from: date)
             datePicker.removeFromSuperview()
             toolBar.removeFromSuperview()
         }
@@ -251,87 +203,61 @@ class JournalViewController : UIViewController{
         return result
     }
     
-    func checkHeatlhy(tanggal: String) -> String {
-        var data : [String] = []
-        
-        for(i) in fetchData.indices{
-            //print("Tanggal: \(fetchData[i].value(forKeyPath: "dateTaken") as! String)")
-            if ((fetchData[i].value(forKeyPath: "dateTaken") as? String) != nil && fetchData[i].value(forKey: "dateTaken") as? String == tanggal){
-                data.append(fetchData[i].value(forKeyPath: "foodStatus") as? String ?? "")
-            }else{
-                return ""
-            }
-        }
-        
-        let healthy = data.lazy.filter{x in x == "Healthy" }.count
-        //let common = data.lazy.filter{x in x == "Common" }.count
-        let unhealthy = data.lazy.filter{x in x == "Unhealthy" }.count
-        
-        if (healthy  > unhealthy){
-            return "Healthy"
-        }else if (healthy < unhealthy){
-            return "Unhealthy"
-        }else if (healthy == 0 && unhealthy == 0 ){
-            return ""
-        }else {
-            return "Unhealthy"
-        }
-    }
-    
-    func foodStatusChange(){
-        
-    }
+//    func checkHeatlhy(tanggal: String) -> String {
+//        var data : [String] = []
+//
+//        for(i) in fetchData.indices{
+//            //print("Tanggal: \(fetchData[i].value(forKeyPath: "dateTaken") as! String)")
+//            if ((fetchData[i].value(forKeyPath: "dateTaken") as? String) != nil && fetchData[i].value(forKey: "dateTaken") as? String == tanggal){
+//                data.append(fetchData[i].value(forKeyPath: "foodStatus") as? String ?? "")
+//            }else{
+//                return ""
+//            }
+//        }
+//
+//        let healthy = data.lazy.filter{x in x == "Healthy" }.count
+//        //let common = data.lazy.filter{x in x == "Common" }.count
+//        let unhealthy = data.lazy.filter{x in x == "Unhealthy" }.count
+//
+//        if (healthy  > unhealthy){
+//            return "Healthy"
+//        }else if (healthy < unhealthy){
+//            return "Unhealthy"
+//        }else if (healthy == 0 && unhealthy == 0 ){
+//            return ""
+//        }else {
+//            return "Unhealthy"
+//        }
+//    }
     
 }
-
 // MARK : - UICollectionViewDataSource
 extension JournalViewController : UICollectionViewDataSource{
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == self.collectionViewWeekly {
             return date.count
-        }else if collectionView == self.collectionViewPhotoGallery{
-            return carouselData.count
         }
         return 0
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+
         if collectionView == self.collectionViewWeekly {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "weeklyCollectionViewCell", for: indexPath) as! WeeklyCollectionViewCell
             
-            //            let numberOfCell = 7
-            //           let cellSpecing = 20
-            //               if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            //                layout.itemSize = CGSize(width: (Int(UIScreen.main.bounds.width) - (cellSpecing * (numberOfCell + 1))) / numberOfCell, height: 50)
-            //                    layout.invalidateLayout()
-            //                }
-            
             cell.setUI(dateText: date[indexPath.item])
-            let mystatus = checkHeatlhy(tanggal: dateForDataBase[indexPath.row])
-            cell.changeUpdate(status: mystatus)
-            
-            return cell
-        } else if collectionView == self.collectionViewPhotoGallery{
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "galleryPhotoCell", for: indexPath) as! GalleryPhotoCollectionViewCell
-            
-            if(indexPath.item == 0){
-                cell.photoGalleryView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-            }
-            
-            cell.setUI(dataPhoto: carouselData[indexPath.item])
-            
+
             return cell
         }
-        
+
         return UICollectionViewCell()
-        
+
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let date = Date()
         let format = DateFormatter()
@@ -340,46 +266,28 @@ extension JournalViewController : UICollectionViewDataSource{
         fetchData = myFetchRequestByDate(date: formattedDate)
         if collectionView == self.collectionViewWeekly {
             let cell = collectionView.cellForItem(at: indexPath) as! WeeklyCollectionViewCell
-            let mystatus = checkHeatlhy(tanggal: dateForDataBase[indexPath.row])
-            cell.changeUpdate(status: mystatus)
-        }else if collectionView == self.collectionViewPhotoGallery{
-            //            let cell = collectionView.cellForItem(at: indexPath) as! GalleryPhotoCollectionViewCell
-            //            if(indexPath.item == 0){
-            //                //pindah ke halaman foodRecog
-            //                performSegue(withIdentifier: "goToFoodRecog", sender: self)
-            //            }else
-            
-            if  indexPath.item <= fetchData.count {
-                //masuk ke halaman detail
-                let food = getFoodFromCoreDataByName(name: fetchData[indexPath.item].value(forKeyPath: "foodName") as! String)
-                let storyboard = UIStoryboard(name: "FoodDetail", bundle: nil);
-                let vc = storyboard.instantiateViewController(withIdentifier: "FoodDetailViewController") as! FoodDetailViewController
-                vc.selectedFood = food
-                vc.imageHasilPhoto = UIImage(data: fetchData[indexPath.item].value(forKeyPath: "foodPhoto") as! Data)
-                vc.modalPresentationStyle = .pageSheet
-                self.present(vc, animated: true, completion: nil)
-            }
         }
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         if collectionView == self.collectionViewWeekly {
             let cell = collectionView.cellForItem(at: indexPath) as! WeeklyCollectionViewCell
-            let mystatus = checkHeatlhy(tanggal: dateForDataBase[indexPath.row])
-            cell.changeUpdate(status: mystatus)
         }
     }
-    
-    //how to equaly space
+
 }
 
-//extension JournalViewController : WeeklyCollectionViewCellProtocol{
-//    func reloadCell() {
-//        DispatchQueue.main.async {
-//            self.collectionViewWeekly.reloadData()
+//extension JournalViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        var widthCell : CGSize = CGSize(width: 100, height: 100)
+//        if collectionView == self.collectionViewWeekly {
+//            widthCell = CGSize(width: 80, height: 60)
 //        }
+//
+//        return widthCell
 //    }
 //}
+
 
 // MARK : - UICollectionViewDelegate
 extension JournalViewController : UICollectionViewDelegate, UIScrollViewDelegate{
@@ -422,43 +330,6 @@ extension JournalViewController : UICollectionViewDelegate, UIScrollViewDelegate
     ////            targetContentOffset.pointee = CGPoint(x: newTargetOffset, y: 0.0)
     //
     //    }
-}
-
-extension JournalViewController : ChartViewDelegate{
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        pieChartView.frame = CGRect(x: 0, y: 0, width: viewPieChart.frame.size.width, height: viewPieChart.frame.size.height)
-        
-        pieChartView.holeRadiusPercent = 0.7
-        pieChartView.transparentCircleRadiusPercent = 0.0
-        pieChartView.drawHoleEnabled = true
-        pieChartView.holeColor = .systemBackground
-        
-        // hides center text
-        pieChartView.drawCenterTextEnabled = true
-        pieChartView.legend.enabled = false
-        pieChartView.drawEntryLabelsEnabled = false
-        pieChartView.entryLabelColor = .clear
-        pieChartView.animate(xAxisDuration: 1.5)
-        pieChartView.setExtraOffsets(left: -15, top: -15, right: -15, bottom: -15)
-        
-        viewPieChart.addSubview(pieChartView)
-        pieChartView.addSubview(stackPieChart)
-        
-        let set = PieChartDataSet(entries: dataEntries)
-        set.setColors(UIColor(red: 0.09, green: 0.54, blue: 0.38, alpha: 1.00), UIColor(red: 0.09, green: 0.84, blue: 0.58, alpha: 1.00), UIColor.red)
-        let data = PieChartData(dataSet: set)
-        
-        dataEntries.removeAll()
-        //append data to pie chart
-        dataEntries.append( PieChartDataEntry(value: Double(count_healthy)))
-        dataEntries.append( PieChartDataEntry(value: Double(count_common)))
-        dataEntries.append( PieChartDataEntry(value: Double(count_unhealthy)))
-        
-        data.setDrawValues(false)
-        pieChartView.data = data
-        
-    }
 }
 
 extension JournalViewController{
